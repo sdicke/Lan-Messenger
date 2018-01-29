5c3e60184ae2d434653c2fc8f57742006b2383a4
Sebastian Martin Dicke <Sebastianmartindicke@gmx.de>
2018-01-29 01:39:24 +0100

    Updated copyright notice.

--- Lan-Messenger/pmwindow.cpp
+++ Lan-Messenger/pmwindow.cpp
@@ -1,5 +1,6 @@
 // Lan Messenger
 // Copyright (C) 2012 Faraz Fallahi <fffaraz@gmail.com>
+// Copyrigh (C) 2017 - 2018 improved by Sebastian Martin Dicke <Sebastianmartindicke [AT] gmx.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
